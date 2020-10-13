60d9eb12604c3a5c31c614fb343a77e32dcfc36e
Angel Rey <afreydev@gmail.com>
2020-10-13 15:28:14 -0500

    Fixed tests

--- alyx/buffalo/tests/test_electrodelogs_bulk_load_tests.py
+++ alyx/buffalo/tests/test_electrodelogs_bulk_load_tests.py
@@ -8,67 +8,74 @@ from buffalo.models import BuffaloSubject, Electrode, ElectrodeLog
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class BulkLoadTests(TestCase):
 
+class ElectrodeLogsBulkLoadTests(TestCase):
     def setUp(self):
         User = get_user_model()
-        my_admin = User.objects.create_superuser('admin', 'admin@test.com', '123456')
+        my_admin = User.objects.create_superuser("admin", "admin@test.com", "123456")
         self.client = Client()
         self.client.force_login(my_admin)
 
-        sam = BuffaloSubject.objects.get_or_create(nickname="Sam")
+        sam = BuffaloSubject.objects.get_or_create(nickname="Sam")[0]
+
         for i in range(1, 97):
             Electrode.objects.get_or_create(subject=sam, channel_number=i)
 
         # Open file
-        self.file_sam_xlsm = open(os.path.join(TEST_DIR + '/files', 'SamAnteriorElectrodeTracking.xlsm'), 'rb')
-        self.file_sam_xlsm_wrong = open(os.path.join(TEST_DIR + '/files', 'SamAnteriorElectrodeTrackingWrong.xlsm'), 'rb')
-    
+        self.file_sam_xlsm = open(
+            os.path.join(TEST_DIR + "/files", "SamAnteriorElectrodeTracking.xlsm"), "rb"
+        )
+        self.file_spock_mat = open(
+            os.path.join(TEST_DIR + "/files", "SpockTrodeInfo.mat"), "rb"
+        )
+        self.file_sam_xlsm_wrong = open(
+            os.path.join(TEST_DIR + "/files", "SamAnteriorElectrodeTrackingWrong.xlsm"),
+            "rb",
+        )
+
     def test_upload_bad_structure(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
-        bulk_load_url = \
-            reverse("electrodelog-bulk-load", kwargs={'subject_id': sam.id})
+        bulk_load_url = reverse("electrodelog-bulk-load", kwargs={"subject_id": sam.id})
         resp = self.client.post(
-            bulk_load_url, 
+            bulk_load_url,
             {
-                'file': self.file_spock_stl_wrong,
-                'subject': sam.id,
-            }
-            , format='multipart'
+                "file": self.file_sam_xlsm_wrong,
+                "subject": sam.id,
+            },
+            follow=True,
+            format="multipart",
         )
-        self.assertContains(resp, "The structure of this file is wrong.")
-    
+        self.assertContains(resp, "Error loading the file - Sheet: Trode (66) - Row: 6 - Column: 2")
+
     def test_upload_bad_file_extension(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
-        bulk_load_url = \
-            reverse("electrodelog-bulk-load", kwargs={'subject_id': sam.id})
+        bulk_load_url = reverse("electrodelog-bulk-load", kwargs={"subject_id": sam.id})
         resp = self.client.post(
-            bulk_load_url, 
+            bulk_load_url,
             {
-                'file': self.file_spock_stl,
-                'subject': sam.id,
-            }
-            , format='multipart'
+                "file": self.file_spock_mat,
+                "subject": sam.id,
+            },
+            format="multipart",
         )
-        self.assertContains(resp, "File extension should be “xlsm”")
+        self.assertContains(resp, "File extension “mat” is not allowed. Allowed extensions are: xlsm.")
 
     def test_upload_well(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
-        bulk_load_url = \
-            reverse("electrodelog-bulk-load", kwargs={'subject_id': sam.id})
+        bulk_load_url = reverse("electrodelog-bulk-load", kwargs={"subject_id": sam.id})
         resp = self.client.post(
-            bulk_load_url, 
+            bulk_load_url,
             {
-                'file': self.file_sam_xlsm,
-                'subject': sam.id,
+                "file": self.file_sam_xlsm,
+                "subject": sam.id,
             },
             follow=True,
-            format='multipart'
+            format="multipart",
         )
         self.assertContains(resp, "File loaded successful")
-        electrode_47 = Electrode.objects.filter(subject=sam, channel_number=47)
+        electrode_47 = Electrode.objects.get(subject=sam, channel_number=47)
         electrodelogs_47 = ElectrodeLog.objects.filter(electrode=electrode_47)
         self.assertEquals(20, len(electrodelogs_47))
-        electrode_74 = Electrode.objects.filter(subject=sam, channel_number=74)
+        electrode_74 = Electrode.objects.get(subject=sam, channel_number=74)
         electrodelogs_74 = ElectrodeLog.objects.filter(electrode=electrode_74)
         self.assertEquals(17, len(electrodelogs_74))
