6f2f4b4daf01b9b7f1b7546178bdcc03f96fb957
Angel Rey <afreydev@gmail.com>
2020-10-28 10:31:06 -0500

    Updated test to support devices

--- alyx/buffalo/tests/test_channel_recording_bulk_load_tests.py
+++ alyx/buffalo/tests/test_channel_recording_bulk_load_tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-from buffalo.models import BuffaloSubject, Electrode
+from buffalo.models import BuffaloSubject, Electrode, Device, ChannelRecording, BuffaloSession
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,9 +16,13 @@ class ChannelRecordingsBulkLoadTests(TestCase):
         self.client.force_login(my_admin)
 
         sam = BuffaloSubject.objects.get_or_create(nickname="Sam")[0]
+        dsamp = Device.objects.get_or_create(name="posterior-sam", subject=sam)[0]
+        dsama = Device.objects.get_or_create(name="anterior-sam", subject=sam)[0]
         # 331 sesiones
         for i in range(1, 125):
-            Electrode.objects.get_or_create(subject=sam, channel_number=i)
+            Electrode.objects.get_or_create(subject=sam, device=dsamp, channel_number=i)
+        for i in range(1, 97):
+            Electrode.objects.get_or_create(subject=sam, device=dsama, channel_number=i)
 
         self.file_sam_xlsx = open(
             os.path.join(TEST_DIR + "/files", "Sam_UnitsTrackingCurrent.xlsx"), "rb"
@@ -33,6 +37,7 @@ class ChannelRecordingsBulkLoadTests(TestCase):
 
     def test_upload_bad_structure(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
+        dsamp = Device.objects.get(name="posterior-sam", subject=sam)
         bulk_load_url = reverse(
             "channelrecord-bulk-load", kwargs={"subject_id": sam.id}
         )
@@ -40,6 +45,7 @@ class ChannelRecordingsBulkLoadTests(TestCase):
             bulk_load_url,
             {
                 "file": self.file_sam_xlsx_wrong,
+                "device": dsamp,
                 "subject": sam.id,
             },
             follow=True,
@@ -49,6 +55,7 @@ class ChannelRecordingsBulkLoadTests(TestCase):
 
     def test_upload_bad_file_extension(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
+        dsamp = Device.objects.get(name="posterior-sam", subject=sam)
         bulk_load_url = reverse(
             "channelrecord-bulk-load", kwargs={"subject_id": sam.id}
         )
@@ -56,6 +63,7 @@ class ChannelRecordingsBulkLoadTests(TestCase):
             bulk_load_url,
             {
                 "file": self.file_spock_mat,
+                "device": dsamp.id,
                 "subject": sam.id,
             },
             format="multipart",
@@ -66,6 +74,7 @@ class ChannelRecordingsBulkLoadTests(TestCase):
 
     def test_upload_well(self):
         sam = BuffaloSubject.objects.get(nickname="Sam")
+        dsamp = Device.objects.get(name="posterior-sam", subject=sam)
         bulk_load_url = reverse(
             "channelrecord-bulk-load", kwargs={"subject_id": sam.id}
         )
@@ -73,6 +82,26 @@ class ChannelRecordingsBulkLoadTests(TestCase):
             bulk_load_url,
             {
                 "file": self.file_sam_xlsx,
+                "device": dsamp.id,
+                "subject": sam.id,
+            },
+            follow=True,
+            format="multipart",
+        )
+        self.assertContains(resp, "File loaded successful")
+
+    def test_upload_well_with_sufix(self):
+        sam = BuffaloSubject.objects.get(nickname="Sam")
+        dsamp = Device.objects.get(name="posterior-sam", subject=sam)
+        bulk_load_url = reverse(
+            "channelrecord-bulk-load", kwargs={"subject_id": sam.id}
+        )
+        resp = self.client.post(
+            bulk_load_url,
+            {
+                "file": self.file_sam_xlsx,
+                "device": dsamp.id,
+                "sufix": 'a',
                 "subject": sam.id,
             },
             follow=True,
